5cae14c634e70188b72909a49759f8843eb224d2
Friiiman <friiiman94@gmail.com>
2024-01-22 11:32:54 +0300

    const/let

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 // Работал вместе с Сергей Курнаков
 
-let perPage = 20;
+const perPage = 20;
 let page = Math.floor(Math.random() * 7) + 1;
 let currentImageIndex = Math.floor(Math.random() * perPage);
 
 const pageMainEl = document.querySelector('.page-main');
 const historyEl = document.querySelector('.history');
+
+const localStorageKey = "likesCount";
 
 // ----------------------------------------------------------------------------
 
@@ -17,12 +19,11 @@ fetch(`https://api.unsplash.com/photos?page=${page}&per_page=${perPage}&client_i
         return response.json();
     })
     .then((data) => {
-        const localStorageKey = "likesCount";
         const currentImage = data[currentImageIndex];
 
         addImageEl(currentImage);
-        likeBtn(localStorageKey, currentImage);
-        likeBtnRise(localStorageKey);
+        likeBtn(currentImage);
+        likeBtnRise();
         saveToHistory(currentImage);
     })
     .catch((error) => {
@@ -40,10 +41,10 @@ function addImageEl(currentImage) {
         </div>`)
 }
 
-function likeBtn(localStorageKey, currentImage) {
+function likeBtn(currentImage) {
     const currentData = [{ id: currentImage.id, likes: currentImage.likes, like: false }];
     const setCurrentData = JSON.stringify(currentData);
-    let allLikes = localStorage.getItem(localStorageKey);
+    const allLikes = localStorage.getItem(localStorageKey);
     if (!allLikes) {
         localStorage.setItem(localStorageKey, setCurrentData);
     }
@@ -65,14 +66,14 @@ function getLikesHtml(element) {
     return `<button class="like-btn">Понравилось</button><p class="like-count">${element.likes}</p>`;
 };
 
-function likeBtnRise(localStorageKey) {
+function likeBtnRise() {
     const likeBtnEl = document.querySelector('.like-btn');
     const likeCountEl = document.querySelector('.like-count');
 
     const getData = JSON.parse(localStorage.getItem(localStorageKey));
     const closestImageElId = likeBtnEl.closest('.image').getAttribute('id');
-    let getCurrentData = getData.filter((element) => element.id === closestImageElId);
-    let getCurrentDataIndex = getData.findIndex(obj => obj.id === closestImageElId);
+    const getCurrentData = getData.filter((element) => element.id === closestImageElId);
+    const getCurrentDataIndex = getData.findIndex(obj => obj.id === closestImageElId);
     if (getCurrentData[0].like) {
         likeBtnEl.disabled = true;
     }
@@ -90,19 +91,19 @@ function likeBtnRise(localStorageKey) {
 }
 
 function saveToHistory(currentImage) {
-    const localStorageKey = "history";
+    const historyLocalStorageKey = "history";
     const currentData = [{ id: currentImage.id, url: currentImage.urls.small, autor: currentImage.user.name }];
     const setCurrentData = JSON.stringify(currentData);
-    let allHistory = localStorage.getItem(localStorageKey);
+    const allHistory = localStorage.getItem(historyLocalStorageKey);
     if (!allHistory) {
-        localStorage.setItem(localStorageKey, setCurrentData);
+        localStorage.setItem(historyLocalStorageKey, setCurrentData);
     }
 
-    const getData = JSON.parse(localStorage.getItem(localStorageKey));
-    let getCurrentData = getData.filter((element) => element.id === currentImage.id);
+    const getData = JSON.parse(localStorage.getItem(historyLocalStorageKey));
+    const getCurrentData = getData.filter((element) => element.id === currentImage.id);
     if (getCurrentData.length === 0) {
         getData.push(...currentData);
-        localStorage.setItem(localStorageKey, JSON.stringify(getData));
+        localStorage.setItem(historyLocalStorageKey, JSON.stringify(getData));
     }
 
     const historyElHtml = getData.map((element) => getHistoryHtml(element)).join("");
